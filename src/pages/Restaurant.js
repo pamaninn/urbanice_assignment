f14ec492ac4eb8088404885cd03f0985b93963f7
pamaninn <pamaninn@gmail.com>
2022-08-04 22:19:15 +0700

    fetch google api

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -5,41 +5,65 @@ function Restaurant() {
   const [location, setLocation] = useState("-33.8670522,151.1957362");
   const [restaurant, setRestaurant] = useState([]);
   const [apiResponse, setAPIResponse] = useState(null);
-  const apiKey = "AIzaSyDF60e9whdj82vQ_qSkLEdSv73KI-y92sM";
+  const [address, setAddress] = useState("บางซื่อ");
+  const apiKey = "AIzaSyCIaAnRX6RU5Ko29T2ukeXbNjlXTyot-ys";
   const radius = "100";
 
-  const url =
-    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-    location +
-    "&radius=" +
-    radius +
-    "&type=restaurant&key=" +
-    apiKey;
-
   const makeAPICall = async () => {
+    const url =
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+      location +
+      "&radius=" +
+      radius +
+      "&type=restaurant&key=" +
+      apiKey;
     console.log(url);
 
-    await fetch(url, {
-      mode: 'no-cors',
-      method: "GET",
-      headers:{
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials':true,
-        'Access-Control-Allow-Methods':'POST, GET',
-        'Content-Type': 'application/json'
-      }
-    }).then((data) => {
-      console.log(data);
+    await fetch(url, {     
+      method: "GET"
+      // headers: {
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Allow-Credentials": true,
+      //   "Access-Control-Allow-Methods": "POST, GET",
+      //   "Content-Type": "application/json",
+      // },
+    })
+      .then((response) => {
+       console.log(response);
+      })
+      .then((data) => {
+        console.log(data);
         setRestaurant(data.results);
         setAPIResponse(data.status);
       });
   };
 
+  const getCoordinates = () => {
+    fetch(
+      "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+        address +
+        "&key=" +
+        apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const latitude = data.results[0].geometry.location.lat;
+        const longitude = data.results[0].geometry.location.lng;
+        console.log({ latitude, longitude });
+        setLocation(latitude + "," + longitude);
+      });
+  };
+
   useEffect(() => {
     makeAPICall();
+    getCoordinates();
   }, [location]);
 
-  return <div>{restaurant} : {apiResponse}</div>;
+  return (
+    <div>
+      {restaurant} : {apiResponse}
+    </div>
+  );
 }
 
 export default Restaurant;
