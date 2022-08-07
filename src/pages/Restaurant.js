d404281d5f6b36471b0eb0b10ba88786a59931dc
pamaninn <pamaninn@gmail.com>
2022-08-08 00:41:31 +0700

    resteurant

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import * as ReactDOM from "react-dom";
 import axios from "axios";
-import { response } from "express";
-
+import RestaurantCard from "../components/RestaurantCard";
 
 function Restaurant() {
   const [location, setLocation] = useState("-33.8670522,151.1957362");
@@ -10,63 +8,31 @@ function Restaurant() {
   const [apiResponse, setAPIResponse] = useState(null);
   const [address, setAddress] = useState("บางซื่อ");
   const apiKey = "AIzaSyCIaAnRX6RU5Ko29T2ukeXbNjlXTyot-ys";
-  const radius = "100";
-  
-  
+  const radius = "500";
 
-  const makeAPICall = async () => {
-
-     const url =
+  //get restaurant with google api
+  const getRestaurant = async () => {
+    const url =
       "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
       location +
       "&radius=" +
       radius +
       "&type=restaurant&key=" +
       apiKey;
-    console.log(url);
-
-   
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
     
-    axios.get('http://localhost:3000').then((response) => {console.log(response);})
-
-    // fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.828253,100.5284507&radius=100&type=restaurant&key=AIzaSyCIaAnRX6RU5Ko29T2ukeXbNjlXTyot-ys", requestOptions)
-    //   .then(response => response.text())
-    //   .then(result => console.log(result))
-    //   .catch(error => console.log('error', error));
-
-    // const url =
-    //   "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-    //   location +
-    //   "&radius=" +
-    //   radius +
-    //   "&type=restaurant&key=" +
-    //   apiKey;
-    // console.log(url);
-
-    // await fetch(url, {     
-    //   method: "GET"
-    //   // headers: {
-    //   //   "Access-Control-Allow-Origin": "*",
-    //   //   "Access-Control-Allow-Credentials": true,
-    //   //   "Access-Control-Allow-Methods": "POST, GET",
-    //   //   "Content-Type": "application/json",
-    //   // },
-    // })
-    //   .then((response) => {
-    //    console.log(response);
-    //   })
-    //   .then((data) => {
-    //     console.log(data);
-    //     setRestaurant(data.results);
-    //     setAPIResponse(data.status);
-    //   });
+    //CALL POST METHOD from localhost:5000 server
+    axios
+      .post("http://localhost:5000/googleapi", { path: url })
+      .then((res) => {
+        setRestaurant(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log("error", error));
   };
 
-  const getCoordinates = () => {
+  //get latitude and longtitude location
+  const getLocations = () => {
+    
     fetch(
       "https://maps.googleapis.com/maps/api/geocode/json?address=" +
         address +
@@ -77,19 +43,25 @@ function Restaurant() {
       .then((data) => {
         const latitude = data.results[0].geometry.location.lat;
         const longitude = data.results[0].geometry.location.lng;
-        console.log({ latitude, longitude });
+        //set value to location
         setLocation(latitude + "," + longitude);
       });
   };
 
   useEffect(() => {
-    makeAPICall();
-    getCoordinates();
+    getRestaurant();
+    getLocations();
   }, [location]);
 
   return (
     <div>
-      {restaurant} : {apiResponse}
+      {restaurant.map((item) => (
+        
+        <RestaurantCard {...item}/>
+      
+      ))
+      }
+    
     </div>
   );
 }
